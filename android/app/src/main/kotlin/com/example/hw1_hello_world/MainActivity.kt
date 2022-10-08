d53e30398ff52baa823eaa5c3bd97e296c610535
Justin Boyer <justinbboyer@gmail.com>
2022-10-08 09:34:50 -0700

    HW1 Done

---
+++ android/app/src/main/kotlin/com/example/hw1_hello_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hw1_hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'HW1 : Hello World'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title:  Text(widget.title)
+          ),
+        body: const Center(
+            child: NavText(pageText: 'Page 1', navPage: SecondPage(title: "Second Page"),),
+        ),
+      );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title:  Text(widget.title)
+      ),
+      body: const Center(
+        child: NavText(pageText: 'Page 2', navPage: HomePage(title: 'HW1 : Hello World')),
+      ),
+    );
+  }
+}
+
+
+class NavText extends StatefulWidget {
+  const NavText({super.key, required this.pageText, required this.navPage});
+  final String pageText;
+  final Widget navPage;
+
+  @override
+  State<NavText> createState() => _NavTextState();
+}
+  
+class _NavTextState extends State<NavText> {
+  void _navigate() {
+    Navigator.of(context).push(
+        MaterialPageRoute<void>(
+            builder: (context){
+              return widget.navPage;
+            }
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Text(widget.pageText),
+        ),
+        Expanded(
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              onPressed: _navigate,
+            )
+        ),
+      ]
+    );
+  }
+}
